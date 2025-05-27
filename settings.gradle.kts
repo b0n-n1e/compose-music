bed1af575e5712a5dfb4585d56f80ef2a5466995
B0nn1e <3110901614@qq.com>
2025-05-27 21:28:18 +0800

    chore:initial

---
+++ settings.gradle.kts
@@ -0,0 +1,43 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+
+        maven { url = uri("https://www.jitpack.io") }
+        maven { url = uri("https://jitpack.io") }
+
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+
+        maven { url = uri("https://www.jitpack.io") }
+        maven { url = uri("https://jitpack.io") }
+
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "compose-music"
+include(":app")
+include(":center")
